7031ce0857cf3ee426cbddcbd6a6fd4d37160096
Ali Memon <alimemon@Alis-MacBook-Pro.local>
2023-04-16 17:42:34 +0500

    Added sidebar with seperate components

--- src/layouts/user_view.tsx
+++ src/layouts/user_view.tsx
@@ -1,17 +1,22 @@
 import React from "react"
+import { Layout } from 'antd';
+import { SideBar } from "@/components/sidebar";
+import { MainContent } from "@/components/main_content";
 import { ResponsiveGrid } from "@/components/responsive_grid";
 
 export const UserView: React.FC = () => {
+
     return <div>
-        <ResponsiveGrid cols={[
-            {
-                size: { sm: 0, md: 6 , lg: 5, xl: 12, xxl: 12 },
-                component: <div>Hello Side Grid</div>
-            },
-            {
-                size: { sm: 24, md: 18, lg: 5, xl: 12, xxl: 12 },
-                component: <div>Hello Grid</div>
-            },
-        ]} />
+        <Layout style={{ minHeight: '100vh' }}>
+            <SideBar />
+            <Layout className="site-layout">
+                <MainContent>
+                    <ResponsiveGrid cols={[{
+                        size: { sm: 24, md: 24, lg: 24, xl: 24, xxl: 24 },
+                        component: <div>GPT List is here</div>
+                    }]} />
+                </MainContent>
+            </Layout>
+        </Layout>
     </div>
 }
