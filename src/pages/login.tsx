053973bccba438682b5804bb17d30df8003c84fe
Ali Memon <alimemon@Alis-MacBook-Pro.local>
2023-04-20 22:54:27 +0500

    Added login page with design

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,22 +3,33 @@ import { GoogleLogin, CredentialResponse } from '@react-oauth/google';
 import { post } from "@/services/http";
 import { googleLogin } from "@/services/endpoints";
 import { AxiosResponse } from "axios";
+import { Card, Typography } from "antd";
 
 type GoogleResponse = {
     credential: string,
     clientId: string,
 }
+const { Title } = Typography;
+
 export default function LoginPage() {
     const loginWithGoogle = useCallback(async (credentialResponse: CredentialResponse) => {
         const response: AxiosResponse<GoogleResponse> = await post(googleLogin, { token: credentialResponse.credential });
         console.log(response)
     }, [])
-    return <div>
-        <GoogleLogin
-            onSuccess={loginWithGoogle}
-            onError={() => {
-                console.log('Login Failed');
-            }}
-        />
+    return <div className="bg">
+                    <Title className="text-center py-8">Welcome to GPT Forum</Title>
+        <div className="space-align-container">
+            <div className="space-align-block">
+                <Card title="SignIn" hoverable={true} bodyStyle={{ width: '400px' }}>
+                    <GoogleLogin
+                        width={"350"}
+                        onSuccess={loginWithGoogle}
+                        onError={() => {
+                            console.log('Login Failed')
+                        }}
+                    />
+                </Card>
+            </div>
+        </div>
     </div>
 }
