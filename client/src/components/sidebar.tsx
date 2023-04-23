f1f01cfd1d936074bdcd939eebb9fcf06a36adad
Zohaad Afzal <zohaad@diyainteractive.com>
2023-04-23 23:58:29 +0200

    create folder initial impl

--- client/src/components/sidebar.tsx
+++ client/src/components/sidebar.tsx
@@ -1,15 +1,66 @@
-import React, { useState } from 'react';
-import { Layout } from 'antd';
-
+import React, { useEffect, useRef, useState } from 'react';
+import { Button, Input, InputRef, Layout, Tag } from 'antd';
+import { PlusOutlined} from '@ant-design/icons';
 import { TreeMenu } from './tree_menu/tree_menu';
+import { post } from '@/services/http';
+import { createFolder } from '@/services/endpoints';
 const { Sider } = Layout;
 
 export const SideBar: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const [inputVisible, setInputVisible] = useState(false);
+  const [folderName, setFolderName] = useState('');
+  const inputRef = useRef<InputRef | null>(null);
+  const showInput = () => {
+    setInputVisible(true);
+  };
+  useEffect(() => {
+    if (inputVisible) {
+      inputRef.current?.focus();
+    }
+  }, [inputVisible]);
+  const handleInputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setFolderName(e.target.value);
+  };
+  const handleInputConfirm = async () => {
+    if (folderName ) {
+     // Api call and Add into state
+     const {data} = await post(createFolder,{name:folderName});
+    }
+    setInputVisible(false);
+    setFolderName('');
+  };
   return (
     <Sider width={300} collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)}>
       <div style={{ height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)' }} />
       <TreeMenu />
+      <div style={{alignItems:'center',justifyContent:'center',flex:1,display:'flex',marginTop:20}}>
+      {/* <Button color='white' ghost style={{alignItems:'center',display:'flex'}}  icon={<PlusOutlined />} type="dashed">New</Button> */}
+
+      {inputVisible ? (
+        <Input
+          ref={inputRef}
+          type="text"
+          size="small"
+          style={{
+            width: 78,
+          }}
+          value={folderName}
+          onChange={handleInputChange}
+          onBlur={handleInputConfirm}
+          onPressEnter={handleInputConfirm}
+        />
+      ) : (
+        <Tag onClick={showInput} style={{
+          borderStyle: 'dashed',
+          color:'white',
+          alignItems:'center',
+          display:'flex'
+        }}>
+          <PlusOutlined /> New
+        </Tag>
+      )}
+      </div>
     </Sider>
   );
 };
