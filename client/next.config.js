3a5d7df08ccab4f314f24b62c749497a78fb6939
Zohaad Afzal <zohaad@diyainteractive.com>
2023-04-28 17:49:42 +0200

    next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -11,7 +11,7 @@ module.exports = {
         source: "/api/auth/signin/google",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "chat.openai.com" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
