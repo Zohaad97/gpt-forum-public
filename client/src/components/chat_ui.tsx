6c96b3fff5f4267ef1c4a66b1deccdb7f4eb0c60
Ali Wahab <ali.wahab@alphamu.com.au>
2023-04-28 18:36:42 +0500

    Added Routing for chat

--- client/src/components/chat_ui.tsx
+++ client/src/components/chat_ui.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { get } from '@/services/http';
+import {get} from '@/services/http';
 import styles from '@/styles/utils.module.scss';
 import Image from 'next/image';
 import GPTAvatar from './gpt_avatar';
-import { Conversation } from '@/types/conversation.type';
+import {Conversation} from '@/types/conversation.type';
 
 type Item = {
   from: string;
@@ -16,7 +16,7 @@ type ChatResponse = {
   avatarUrl: '';
 };
 
-export const ChatUI: React.FC<{ chat: Conversation }> = ({ chat }) => {
+export const ChatUI: React.FC<{chat: Conversation}> = ({chat}) => {
   console.log(chat);
 
   // useEffect(() => {
@@ -34,25 +34,26 @@ export const ChatUI: React.FC<{ chat: Conversation }> = ({ chat }) => {
   //   console.log(chat);
   // }, [chat]);
   return (
-    <div className="flex flex-col items-center pb-24 dark:bg-[#343541] min-h-screen">
+    <div className="flex flex-col items-center dark:bg-[#343541] min-h-screen">
       {chat.items &&
         chat.items.map((item: Item, idx) => {
           return (
             <div
               id={idx.toString()}
               key={item.value}
-              className={`relative dark:bg-[#343541] text-gray-700 w-full border-b dark:border-gray-700 border-gray-200 ${item.from === 'gpt' ? 'bg-gray-100 dark:bg-[#434654]' : ''
-                }`}
+              className={`relative dark:bg-[#343541] text-gray-700 w-full border-b dark:border-gray-700 border-gray-200 ${
+                item.from === 'gpt' ? 'bg-gray-100 dark:bg-[#434654]' : ''
+              }`}
             >
               <div className="relative mx-auto max-w-screen-xl dark:text-gray-100 text-gray-700 w-full px-4 py-10">
                 <div className="w-full max-w-screen-md flex flex-1 mx-auto gap-[1.5rem] leading-[1.75] whitespace-pre-wrap">
-                  {item.from === "human" ? (
+                  {item.from === 'human' ? (
                     <Image
                       className="mr-2 rounded-sm h-[28px]"
                       alt="Avatar of the person chatting"
                       width="28"
                       height="28"
-                      src='https://abc.xyz/favicon-194x194.png'
+                      src="https://abc.xyz/favicon-194x194.png"
                     />
                   ) : (
                     <GPTAvatar model={'GPT-3'} />
@@ -63,7 +64,7 @@ export const ChatUI: React.FC<{ chat: Conversation }> = ({ chat }) => {
                     ) : (
                       <div
                         className={styles.response}
-                        dangerouslySetInnerHTML={{ __html: item.value }}
+                        dangerouslySetInnerHTML={{__html: item.value}}
                       />
                     )}
                   </div>
--- client/src/components/tree_menu/tree_menu.tsx
+++ client/src/components/tree_menu/tree_menu.tsx
@@ -1,117 +1,107 @@
-import React, { useEffect, useState } from 'react';
-import { Space, Tree } from 'antd';
-import type { DataNode, EventDataNode, TreeProps } from 'antd/es/tree';
-import { get, put } from '@/services/http';
-import { getAllFolders, updateChatFolder } from '@/services/endpoints';
-import styles from "./tree_menu.module.scss"
-import { Key } from 'antd/es/table/interface';
-import { observer } from 'mobx-react';
-import { ChatStore } from '@/stores/chat_store';
+import React, {useEffect, useReducer, useState} from 'react';
+import {Space, Tree} from 'antd';
+import type {DataNode, EventDataNode, TreeProps} from 'antd/es/tree';
+import {get, put} from '@/services/http';
+import {getAllFolders, updateChatFolder} from '@/services/endpoints';
+import styles from './tree_menu.module.scss';
+import {Key} from 'antd/es/table/interface';
+import {observer} from 'mobx-react';
+import {ChatStore} from '@/stores/chat_store';
+import {useRouter} from 'next/router';
 
-const { DirectoryTree } = Tree;
+const {DirectoryTree} = Tree;
 
 export type Folders = {
-    id: number,
-    name: string,
-    conversations: Conversations[]
-}
+  id: number;
+  name: string;
+  conversations: Conversations[];
+};
 
 export type Conversations = {
-    id: number,
-    title: string,
-}
+  id: number;
+  title: string;
+};
 
-// type TreeNode = {
-//     key: string,
-//     title: string,
-//     isLeaf?: boolean
-//     draggable?:boolean;
-//     children?: TreeNode[]
-// }
+type TreeNode = {
+  key: string;
+  title: string;
+  isLeaf?: boolean;
+  draggable?: boolean;
+  children?: TreeNode[];
+};
 
 type SelectEvent = {
-    event: string,
-    selected: boolean,
-    node: EventDataNode<DataNode>
-    selectedNodes: DataNode[],
-    nativeEvent: MouseEvent
-}
+  event: string;
+  selected: boolean;
+  node: EventDataNode<DataNode>;
+  selectedNodes: DataNode[];
+  nativeEvent: MouseEvent;
+};
 
 function modifyResponseAccordingToTree(data: Folders[]) {
-    let tree: TreeNode[] = [];
-    data.forEach((elem: Folders) => {
-        let childs: TreeNode[] = []
-        elem.conversations.forEach((child) => {
-            childs.push({ key: `${child.id}`, title: child.title, isLeaf: true })
-        })
-        tree.push({ key: `folder-${elem.id}`, title: elem.name, children: childs,draggable:false })
-    })
-    return tree
+  let tree: TreeNode[] = [];
+  data.forEach((elem: Folders) => {
+    let childs: TreeNode[] = [];
+    elem.conversations.forEach(child => {
+      childs.push({key: `${child.id}`, title: child.title, isLeaf: true});
+    });
+    tree.push({key: `folder-${elem.id}`, title: elem.name, children: childs, draggable: false});
+  });
+  return tree;
 }
 
 export const TreeMenu: React.FC = observer(() => {
-    const [gData, setGData] = useState<TreeNode[]>([]);
+  const router = useRouter();
+  const [gData, setGData] = useState<TreeNode[]>([]);
 
-    function convertFolderNameToKey (name: number | string) {
-        return Number(name.toString().split("-")[1])
+  function convertFolderNameToKey(name: number | string) {
+    return Number(name.toString().split('-')[1]);
+  }
+  useEffect(() => {
+    (async () => {
+      const response = await get(getAllFolders);
+      ChatStore.setChatFolders(response.data);
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (ChatStore.chatFolders) {
+      const data = modifyResponseAccordingToTree(ChatStore.chatFolders);
+      setGData(data);
     }
-    useEffect(() => {
-        (async () => {
-            const response = await get(getAllFolders);
-            ChatStore.setChatFolders(response.data);
-        })()
-    }, [])
+  }, [ChatStore.chatFolders]);
 
-    useEffect(() => {
-        if(ChatStore.chatFolders){
-            const data = modifyResponseAccordingToTree(ChatStore.chatFolders)
-            setGData(data)
-        }
-    },[ChatStore.chatFolders])
+  const onDragEnter: TreeProps['onDragEnter'] = info => {};
 
-    const onDragEnter: TreeProps['onDragEnter'] = (info) => {
+  const onDrop: TreeProps['onDrop'] = async info => {
+    const chatId = Number(info.dragNode.key);
+    const folderId = convertFolderNameToKey(info.node.key);
+    await put(updateChatFolder(chatId, folderId), {});
+    const response = await get(getAllFolders);
+    ChatStore.setChatFolders(response.data);
+  };
 
-    };
-
-    const onDrop: TreeProps['onDrop'] = async (info) => {
-        console.log(info);
-        const chatId = Number(info.dragNode.key);
-        const folderId = convertFolderNameToKey(info.node.key)
-        await put(updateChatFolder(chatId, folderId),{})
-        const response = await get(getAllFolders);
-        ChatStore.setChatFolders(response.data);     
-    };
-
-    function onSelectItem(selectedKeys: Key[], e: SelectEvent) {      
-        if (!e.node.children) {
-            ChatStore.updateActiveChatId(Number(e.node.key));
-        }
+  function onSelectItem(selectedKeys: Key[], e: SelectEvent) {
+    if (e.node.title && typeof e.node.title === 'string') {
+      const url = `${e.node.title?.toLowerCase().replace(/[^a-zA-Z0-9_-]/g, '-')}/${e.node.key}`;
+      router.push(`/chat/${url}`);
     }
-    const Title = (props:any) => {
-        console.log("render");
-        return <h5>{props.title}</h5>;
-      };
-    return (
-        <DirectoryTree
-            rootClassName={styles['tree-background']}
-            draggable={({isLeaf}) => isLeaf === true}
-            onDragEnter={onDragEnter}
-            onDrop={onDrop}
-            treeData={gData}
-            onSelect={onSelectItem}
-            titleRender={(nodeData) => {
-                return <Title title={nodeData.title} />;
-              }}
-            // titleRender={(node) => {
-            //     if(node.isLeaf){
-            //         return (<Space>
-            //             <span>{node.title}</span>
-            //         </Space>)
-            //     }
-                
-            //     return (<>{node.title?.toString()}</>)
-            //     // console.log({node:node.title})
-            //     }}
-        />
-    );
+  }
+
+  const Title = (props: any) => {
+    return <h5>{props.title}</h5>;
+  };
+  return (
+    <DirectoryTree
+      rootClassName={styles['tree-background']}
+      draggable={({isLeaf}) => isLeaf === true}
+      onDragEnter={onDragEnter}
+      onDrop={onDrop}
+      treeData={gData}
+      onSelect={onSelectItem}
+      titleRender={nodeData => {
+        return <Title title={nodeData.title} />;
+      }}
+    />
+  );
 });
