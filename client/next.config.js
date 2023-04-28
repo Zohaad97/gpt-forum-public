1efb63e3e716f9d734360c1fbf260b890e12b764
Zohaad Afzal <zohaad@diyainteractive.com>
2023-04-28 17:52:15 +0200

    next.config.json

--- client/next.config.js
+++ client/next.config.js
@@ -27,7 +27,7 @@ module.exports = {
         source: "/api/auth/signin",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "chat.openai.com" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
@@ -43,7 +43,7 @@ module.exports = {
         source: "/api/auth/session",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "chat.openai.com" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
@@ -59,7 +59,7 @@ module.exports = {
         source: "/api/auth/csrf",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "chat.openai.com" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
