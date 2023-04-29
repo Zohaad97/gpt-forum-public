9b7a8442b9d7620f3cbc9cf1347e4efc45007e05
Zohaad Afzal <zohaad@diyainteractive.com>
2023-04-29 15:10:59 +0200

    next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -24,6 +24,38 @@ module.exports = {
         ],
       },
       {
+        source: "/api/conversation",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "chrome-extension://gokpglbkijcipcnccebeiklomlncoemn" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+      {
+        source: "/api/conversation-folder",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "chrome-extension://gokpglbkijcipcnccebeiklomlncoemn" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+      {
         source: "/api/auth/signin",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
